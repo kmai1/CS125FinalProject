ba9912343831415eabd8e6a909c3b0ce52b4a2d5
kmai1 <maikevin123@gmail.com>
2018-12-01 19:37:01 -0600

    has button to switch between activities

---
+++ app/src/main/java/com/example/kmai/cs125finalproject/foodScreen.java
@@ -0,0 +1,13 @@
+package com.example.kmai.cs125finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class foodScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_screen);
+    }
+}
